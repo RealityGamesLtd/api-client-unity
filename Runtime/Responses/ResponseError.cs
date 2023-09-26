30184fa0bbc6bb4a351bdc2dd846cc4f5514ac61
Mateusz Młynek <>
2023-09-26 10:32:24 +0200

    Added a boolean to check whether a default error code was assigned

--- Runtime/Responses/ResponseError.cs
+++ Runtime/Responses/ResponseError.cs
@@ -5,16 +5,19 @@ namespace ApiClient.Runtime
         public ResponseError(T errorCode)
         {
             ErrorCode = errorCode;
+            NonDefaultErrorCodeAssigned = !errorCode.Equals(default(T));
         }
 
         public ResponseError(T errorCode, string userFacingErrorMessage)
         {
             ErrorCode = errorCode;
             UserFacingErrorMessage = userFacingErrorMessage;
+            NonDefaultErrorCodeAssigned = !errorCode.Equals(default(T));
         }
 
         public T ErrorCode { get; private set; }
         public string UserFacingErrorMessage { get; private set; }
+        public bool NonDefaultErrorCodeAssigned { get; private set; }
 
         public override string ToString()
         {
