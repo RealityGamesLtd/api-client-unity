ca0a242d37393c5355d69e5789a02d6160f8c1ee
Mateusz Młynek <>
2026-01-26 16:12:30 +0100

    deleted duplicated DefaultApiClientMiddleware class

--- Runtime/ApiClient.cs
+++ Runtime/ApiClient.cs
@@ -998,24 +998,5 @@ namespace ApiClient.Runtime
         }
 
         #endregion
-
-        /// <summary>
-        /// Default middleware
-        /// </summary>
-
-        private sealed class DefaultApiClientMiddleware : IApiClientMiddleware
-        {
-#pragma warning disable CS1998 // allow to run synchronusly
-            public async Task ProcessRequest(IHttpRequest request, bool isResponseWithBackoff = false)
-            {
-
-            }
-
-            public async Task<IHttpResponse> ProcessResponse(IHttpResponse response, string requestId, bool exhaustedRetries = false)
-            {
-                return response;
-            }
-#pragma warning restore CS1998 // allow to run synchronusly
-        }
     }
 }
